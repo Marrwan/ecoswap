2f145d444e278bf7ea66924ebda272c17e3d3386
marrwan <abdulbasitdamilola6@gmail.com>
2023-03-26 13:36:45 +0100

    changes

--- src/component/MonkeList.js
+++ src/component/MonkeList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  {Stack }from '@mui/material';
+import  {Stack , Card}from '@mui/material';
 import Monkes from './Monkes';
 import Monke1 from '../images/GenZ Monke.png';
 import Monke2 from '../images/GenZ Monke(2).png';
@@ -11,12 +11,15 @@ export default function MonkeList(props) {
   {monke: Monke2,  title : "Gen Z Monke", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
   ,{ monke: Monke3, title : "Gen Z Monke", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}];
 
-  const compmonke = monkes.map(monkey=>{
-    return<Monkes title={monkey.title} monke={monkey.monke} key={monkey.monke} description={monkey.description} />;
-  })
+  const compmonke = monkes.map((monkey)=>(
+    <Monkes title={monkey.title} monke={monkey.monke} key={monkey.monke} description={monkey.description} />
+  ))
  return (
-         <Stack direction="row" spacing={2}>
+        //  <Stack direction="row" spacing={2}>
+        <Card sx={{display: 'flex'}}>
+
          {compmonke}
-</Stack>
+        </Card>
+// </Stack>
  )
 }
