4e302b1810b2bc4e7685765214dfc240c511957a
marrwan <abdulbasitdamilola6@gmail.com>
2023-03-27 11:56:43 +0100

    Add typography to Ecoswap component for better UI readability

--- src/component/Ecoswap.js
+++ src/component/Ecoswap.js
@@ -1,2 +1,16 @@
 import React from 'react';
-import {Container} from '@mui/material';
+import {Container, Typography} from '@mui/material';
+
+export default function Ecoswap(){
+    return
+    (
+        <Container>
+            <Container>
+                <Typography>Ecoswap</Typography>
+                <Typography>Swap, earn and build on the leading decentralized 
+cryptocurrency protocol. Best NFTxDEFI on SUI</Typography>
+            </Container>
+            <Container></Container>
+        </Container>
+    )
+}
