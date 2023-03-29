db8913b3d5579586edbbf57339d6180ecdc13007
marrwan <abdulbasitdamilola6@gmail.com>
2023-03-29 06:19:01 +0100

    Add missing Gen Z Monke image to MonkeList componentThe commit adds the missing Gen Z Monke image to the MonkeList component by uncommenting the import statement and updating the list of monkes

--- src/component/MonkeList.js
+++ src/component/MonkeList.js
@@ -3,14 +3,14 @@ import  { Card, Grid, Box}from '@mui/material';
 import Monkes from './Monkes';
 import Monke1 from '../images/GenZ Monke.png';
 import Monke2 from '../images/GenZ Monke(2).png';
-// import Monke3 from '../images/GenZ Monke(3).png';
+import Monke3 from '../images/GenZ Monke(3).png';
 
 
 export default function MonkeList(props) {
     const monkes = [
       {monke: Monke1, title : "Gen Z Monke 1", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"},
   {monke: Monke2,  title : "Gen Z Monke 2", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
-  // ,{ monke: Monke3, title : "Gen Z Monke", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
+  ,{ monke: Monke3, title : "Gen Z Monke", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
 ];
 
   const compmonke = monkes.map((monkey)=>(
