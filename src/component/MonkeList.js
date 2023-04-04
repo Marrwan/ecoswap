dd0ce61d055da85c3bde8ac81072f109163cdd11
Matrix200086 <oyebamijimohammed@gmail.com>
2023-04-04 20:45:05 +0100

    reduced the number of nft viewed by default except is user wants to see more

--- src/component/MonkeList.js
+++ src/component/MonkeList.js
@@ -1,21 +1,29 @@
-import React from "react";
-import  { Card, Grid, Box}from '@mui/material';
+import React, {useState} from "react";
+import  { Card, Grid, Box, Typography}from '@mui/material';
+// import {ExpandMoreIcon, ExpandLessIcon} from '@mui/icons-material';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Monkes from './Monkes';
 import Monke1 from '../images/GenZ Monke.png';
 import Monke2 from '../images/GenZ Monke(2).png';
 import Monke3 from '../images/GenZ Monke(3).png';
 import Monke4 from '../images/GenZ Monke(4).png';
+import Monke5 from '../images/GenZ Monke(5).png';
 
 
 export default function MonkeList(props) {
+  const [expand, setExpand] = useState(false)
     const monkes = [
       {monke: Monke1, title : "Gen Z Monke 1", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"},
   {monke: Monke2,  title : "Gen Z Monke 2", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
   ,{ monke: Monke3, title : "Gen Z Monke", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"},
   {monke: Monke4, title: "Gen Z Monke 4", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
+,  {monke: Monke5, title: "Gen Z Monke 5", description: "GenZ Monkesby Softchain Labs is an utility collection with the best in class art. We’re  building a suite of NFT dApps that are scalable, useful and easy to use for Sui Network. With the extensive experience and success on Near Blockchain, we’re ready to take Sui NFTs to the Next Level"}
 ];
-
-  const compmonke = monkes.map((monkey)=>(
+const change = () =>{
+  setExpand(!expand)
+}
+  const compmonke = (!expand ? monkes.slice(0,2) : monkes).map((monkey)=>(
     <Monkes title={monkey.title} monke={monkey.monke} key={monkey.monke} description={monkey.description} />
   
   ))
@@ -29,6 +37,21 @@ export default function MonkeList(props) {
     >
          {compmonke}
    </Grid>
+   {!expand ? 
+   <Typography component="p" sx={{display: 'flex', alignContent: 'center', cursor: 'pointer'}} onClick={change}>
+
+   See more
+   <ExpandMoreIcon />
+   </Typography>
+   
+   : 
+   <Typography component="p" sx={{display: 'flex', alignContent: 'center', cursor: 'pointer'}} onClick={change}>
+
+    See less
+    <ExpandLessIcon />
+   
+   </Typography>
+}
    </Box>
         
  )
--- src/component/Monkes.js
+++ src/component/Monkes.js
@@ -9,10 +9,10 @@ export default function Monkes(props) {
 
   return (
 <Grid item lg={6} md={6} sm={12} xs={12} sx={{width:"100%"}}>
-    <Card sx={{ display: 'flex', maxWidth: "100%", m: "10px" }}>
+    <Card sx={{ display: 'flex', maxWidth: "100%", m: "10px", borderRadius: 5 }}>
        <CardMedia
         component="img"
-        sx={{ width: 191, borderRadius: 5, m: 0.5, height:{md : "1%", xs:"150px", sm: "170px"} }}
+        sx={{ width: 191, borderRadius: 5, m: 0.5, height:{md : "1%", xs:"120px", sm: "170px"} }}
         image={props.monke}
       
         alt={props.description}
@@ -22,7 +22,7 @@ export default function Monkes(props) {
           <Typography component="div" variant="h5" sx={{marginTop: 0, fontSize:{md: "20px", xs:"15px"}}}>
             {props.title}
           </Typography>
-          <Typography variant="subtitle1" sx={{maxHeight: "100px", fontSize: {xs : "8px", sm: "10px", md: "14px"}}} color="text.secondary" component="p">
+          <Typography variant="subtitle1" sx={{ fontSize: {xs : "5px", sm: "10px", md: "14px"}}} color="text.secondary" component="p">
             {props.description}
           </Typography>
         </CardContent>
