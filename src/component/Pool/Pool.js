a4196acb8aec931456ad654ede496286f0603eb7
marrwan <abdulbasitdamilola6@gmail.com>
2023-05-06 08:50:08 +0100

    mm

--- src/component/Pool/Pool.js
+++ src/component/Pool/Pool.js
@@ -13,7 +13,7 @@ export default function Pool(){
             <Box 
             sx={{
                 marginLeft: '20vw',
-                width: '50vw'
+                // width: '50vw'
             }}
             >
                 <Typography component="img" src={MonkeLogo} sx={{
